311e7e131b2133107079d2eaad1ebc29808b7f54
volody314 <volody314@yandex.ru>
2022-05-09 22:39:41 +1000

    dev1 Downgrade java до версии 11, отлажена компиляция

--- src/test/java/com/volody314/coffeehouse/CoffeehouseApplicationTests.java
+++ src/test/java/com/volody314/coffeehouse/CoffeehouseApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CoffeehouseApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
